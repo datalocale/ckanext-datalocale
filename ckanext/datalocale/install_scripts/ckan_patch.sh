31dd04951b53c995174abc3dbb109b7794bf554f
Alexandre Da Silva <alexandre.dasilva@atos.net>
2012-12-28 16:35:15 +0100

    Ajout du patch pour l'extension spatial.

--- ckanext/datalocale/install_scripts/ckan_patch.sh
+++ ckanext/datalocale/install_scripts/ckan_patch.sh
@@ -4,3 +4,4 @@
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/get.patch
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/group.patch
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/i18n.patch
+patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/spatial.patch
