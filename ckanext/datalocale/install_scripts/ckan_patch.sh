fb7fa8d756b02a19404a0424850ca7ee525ab27e
Oceane Ventura <oceane.ventura@atos.net>
2013-01-18 17:03:51 +0100

    Ajout de divers patch afin d'adapter le comportement de ckan avec datalocale.

--- ckanext/datalocale/install_scripts/ckan_patch.sh
+++ ckanext/datalocale/install_scripts/ckan_patch.sh
@@ -5,3 +5,6 @@ patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/get.patch
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/group.patch
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/i18n.patch
 patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/spatial.patch
+patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/datastore_api.patch
+patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/home.patch
+patch -p1 < ckanext-datalocale/ckanext/datalocale/install_scripts/search.patch
